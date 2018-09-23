981b8a096bb71544d5063303588715c63fbad456
Christoph Michel <christoph.michel@quantilope.com>
2018-09-23 12:08:56 +0100

    Improve UI

---
+++ frontend/src/SimpleTable.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const styles = {
+  root: {
+    width: '100%',
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+};
+
+let id = 0;
+function createData(name, protein) {
+  id += 1;
+  return { id, name, protein };
+}
+
+const data = [
+  createData('FrozenYoghurt', 4.0),
+  createData('IceBabed', 4.3),
+  createData('Eglair', 6.0),
+  createData('Cupie', 4.3),
+  createData('Gingee', 3.9),
+  createData('Basix', 2.1),
+  createData('Enermi', 6.6),
+];
+
+function SimpleTable(props) {
+  const { classes } = props;
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Username</TableCell>
+            <TableCell numeric>Rating</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(n => {
+            return (
+              <TableRow key={n.id}>
+                <TableCell component="th" scope="row">
+                  {n.name}
+                </TableCell>
+                <TableCell numeric>{n.protein}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+SimpleTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTable);
